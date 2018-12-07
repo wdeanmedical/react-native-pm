5125f3a6461456a6b3092f651c2e95f7ee74c84b
Nick Sophinos <nick@wdeanmedical.com>
2018-12-06 23:07:25 -0500

    redux thunk and redux logging added

--- src/config/store.js
+++ src/config/store.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
 import reducers from '../reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
-const store = createStoreWithMiddleware(reducers)
+const middleware = [ReduxThunk]
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { createLogger } = require('redux-logger')
+  const logger = createLogger({
+    collapsed: (getState, action) => action.type === APP_INIT,
+    duration: true,
+  })
+  middleware.push(logger)
+}
+
+const store = createStore(reducers, {}, applyMiddleware(...middleware))
 
 export default store
