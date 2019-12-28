ecdcf4a2b3e907daf0d34a3a078768ac3033908a
Nick Sophinos <nick@wdeanmedical.com>
2019-12-27 22:42:23 -0500

    prior to experimenting with other pickers

--- src/components/FormSelectInput/form_select_input_styles.js
+++ src/components/FormSelectInput/form_select_input_styles.js
@@ -16,26 +16,13 @@ SelectInputStyled.itemLabel = styled.Text`
 SelectInputStyled.select = styled.Picker`
   background-color: ${props =>
     props.submitted ? Colors.lightGray : Colors.white};
-  border-width: 1px;
+  border-width: 0;
   width: 100%;
   font-size: 14px;
   color: ${Colors.black};
   height: 34px;
   padding-left: 10px;
   padding-right: 10px;
-  border-radius: 10px;
-  border-color: ${Colors.textInputBorderColor};
-  &:hover {
-    border: 1px solid ${Colors.azure};
-  }
-  &:focus {
-    border: 1px solid ${Colors.azure};
-  }
-  &:disabled {
-    border: ${Colors.textInputBorderColor};
-    background: ${Colors.textInputDisabledBackground};
-    color: ${Colors.battleshipGrey};
-  }
   &:placeholder {
     color: ${Colors.inputPlaceholderColor};
     font-size: 14px;
